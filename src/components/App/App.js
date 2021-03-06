cdee8dc496c100bf643b9350cbe69cbe489c4c68
mifta10 <miftauddinkhan10@gmail.com>
2021-03-07 02:54:35 +0600

    footer worked

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -23,6 +23,8 @@ import UserPrescription from '../Dashboard/Sidebar/UserPrescription';
 import EditAdmin from '../Admin/EditAdmin/EditAdmin';
 import EditExpert from '../Admin/EditExpert/EditExpert';
 import NewPost from '../Post/NewPost';
+import Cookies from '../Shared/Footer/Cookies';
+import Static from '../Shared/Footer/Static';
 
 export const UserContext = createContext();
 function App() {
@@ -47,6 +49,8 @@ function App() {
           <Route path='/editAdmin/:adminID'> <EditAdmin /> </Route>
           <Route path='/editExpert/:expertID'> <EditExpert /> </Route>
           <Route path='/post'> <NewPost /> </Route>
+          <Route path='/cookies'> <Cookies /> </Route>
+          <Route path='/static'> <Static /> </Route>
         </Switch>
       </Router>
     </UserContext.Provider>
