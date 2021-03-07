65ef0098252c5b4c65e07d01b663c09f5e1ed681
mifta10 <miftauddinkhan10@gmail.com>
2021-03-07 14:29:17 +0600

    Post details page added

--- src/components/Post/PostDetails.js
+++ src/components/Post/PostDetails.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
+import NavBar from '../Shared/NavBar/NavBar';
 
 const PostDetails = () => {
   const { postID } = useParams()
@@ -18,9 +19,29 @@ const PostDetails = () => {
 
 
   return (
-    <div>
-      <h1>hi{postID}</h1>
+    <div className="common__bg__cyan">
+      <NavBar />
+    <div className="container mt-5">
+      <div className="card">
+     <div className="row mt-3 text-center">
+       <div className="col-md-12">
+       <img src={postDetails.img} width={"30%"} />
+       </div>
+     </div>
+     <div className="row text-center">
+       <div className="col-md-12 col-md-offset-2">
+       <h3>{postDetails.post}</h3>
+     </div>
+     </div>
+     <div className="row text-center">
+       <div className="col-md-12">
+       <h3>Posted On:{postDetails.postDate}</h3>
+       <p>Posted By:{postDetails.postName}</p>
+     </div>
+     </div>
+     </div>
       <div class="fb-comments" data-href={`https://mentex-5b2ed.web.app/${postID}`} data-width="300px" data-numposts="5">comment</div>
+    </div>
     </div>
   );
 };
